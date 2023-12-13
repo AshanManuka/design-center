ff613b06b7b0eb29987c878d636a1cd80cbea810
ashan_manuka <manukajayarathne.coma@gmail.com>
2023-12-13 23:05:28 +0530

    save product & product category

--- src/main/java/com/designCenter/designCenter/controller/admin/AdminProductController.java
+++ src/main/java/com/designCenter/designCenter/controller/admin/AdminProductController.java
@@ -1,11 +1,12 @@
 package com.designCenter.designCenter.controller.admin;
 
+import com.designCenter.designCenter.dto.common.CommonResponse;
+import com.designCenter.designCenter.dto.product.ProductReqDto;
 import com.designCenter.designCenter.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/admin/product")
@@ -14,6 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminProductController {
 
     private final ProductService productService;
+
+
+    @PostMapping(value = "/save")
+    public ResponseEntity<?> saveProduct(@ModelAttribute ProductReqDto productRequest,
+                                         @RequestParam long categoryId){
+        log.info("Save new product-Name:{}",productRequest.getProductName());
+        productService.saveProduct(categoryId,productRequest);
+        return ResponseEntity.ok(new CommonResponse<>(true,"Product saved successfully..!"));
+    }
 
 
 }
